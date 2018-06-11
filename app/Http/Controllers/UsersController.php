0481ecb2e2977b8c229e753a15288adca227f5f9
lhl <1043891613@qq.com>
2018-06-11 09:25:18 +0000

    xiaoM

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,15 @@ use Auth;
 use App\Models\User;
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => [ 'create', 'store']
+        ]);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
     //
     public function create(){
 
@@ -14,7 +23,29 @@ class UsersController extends Controller
     }
 
     public function show(User $user){
+        $this->authorize('update', $user);
       return view('users.show',compact('user'));
+    }
+    public function edit(User $user){
+        $this->authorize('update', $user);
+        return view('users.edit',compact('user'));
+    }
+    public function update(User $user,Request $request){
+
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+        $this->authorize('update', $user);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
     }
     public function store(Request $request){
         $this->validate($request, [
